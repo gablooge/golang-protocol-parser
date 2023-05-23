ec384698b9c6ba281e960481a293df60b036e9c3
Samsul Hadi <samsulhadikediri@gmail.com>
2023-05-23 15:54:27 +0700

    feat: feat-zigbee

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func printGoose() {
 func printBLE() {
 	fmt.Println("======= Welcome BLE =======")
 
-	bleFileHandle, err := pcap.OpenOffline("pcaps/ble.pcap")
+	bleFileHandle, err := pcap.OpenOffline("pcaps/blev2.pcap")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -64,6 +64,59 @@ func printBLE() {
 	}
 }
 
+func printZigbee() {
+	fmt.Println("======= Welcome Zigbee =======")
+	zigbeeFileHandle, err := pcap.OpenOffline("pcaps/zigbee.pcap")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer zigbeeFileHandle.Close()
+
+	packetSource := gopacket.NewPacketSource(zigbeeFileHandle, LayerTypeZigbee)
+
+	i := 0
+	// Loop through packets in file
+	for packet := range packetSource.Packets() {
+		layersdata := packet.Layer(LayerTypeZigbee)
+		if layersdata != nil {
+			zigbeeLayerData, _ := layersdata.(*Zigbee)
+			i = i + 1
+			fmt.Println("======= Packet " + strconv.Itoa(i) + " =======")
+			fmt.Println("zigbee.etherType : ", zigbeeLayerData.EthernetType)
+			fmt.Println("zigbee.headerData : ", zigbeeLayerData.HeaderData)
+			fmt.Println("zigbee.Info.Radius : ", zigbeeLayerData.Info.Radius)
+		}
+
+	}
+}
+
+func printLorawan() {
+	fmt.Println("===== Welcome LoRawan =====")
+	lorawanFileHandle, err := pcap.OpenOffline("pcaps/lorawan.pcapng")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lorawanFileHandle.Close()
+
+	packetSource := gopacket.NewPacketSource(lorawanFileHandle, LayerTypeLorawan)
+
+	i := 0
+	// Loop through packets in file
+	for packet := range packetSource.Packets() {
+		layersdata := packet.Layer(LayerTypeLorawan)
+		if layersdata != nil {
+			lorawanLayerData, _ := layersdata.(*Lorawan)
+			i = i + 1
+			fmt.Println("======= Packet " + strconv.Itoa(i) + " =======")
+			fmt.Println("lorawan.etherType : ", lorawanLayerData.EthernetType)
+			fmt.Println("lorawan.headerData : ", lorawanLayerData.HeaderData)
+			fmt.Println("lorawan.Info.Port : ", lorawanLayerData.LorawanInfo.FPort)
+			fmt.Println("lorawan.Info.Payload : ", lorawanLayerData.LorawanInfo.FrmPayload)
+		}
+
+	}
+}
+
 func main() {
 	// printGoose()
 	printBLE()
@@ -73,4 +126,7 @@ func main() {
 	// TODO: lorawan
 
 	fmt.Println("======= Done =======")
+
+	printZigbee()
+	printLorawan()
 }
