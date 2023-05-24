d4b562ecc980e05afb956160f66e5876829f0aa5
Samsul Hadi <samsulhadikediri@gmail.com>
2023-05-24 14:11:30 +0700

    feat: clean modbus

--- layers.go
+++ layers.go
@@ -3,10 +3,9 @@ package main
 import "github.com/google/gopacket"
 
 var (
-	LayerTypeGoose     = gopacket.RegisterLayerType(1001, gopacket.LayerTypeMetadata{Name: "GOOSE", Decoder: gopacket.DecodeFunc(decodeGoose)})
-	LayerTypeBLE       = gopacket.RegisterLayerType(1002, gopacket.LayerTypeMetadata{Name: "BLE", Decoder: gopacket.DecodeFunc(decodeBLE)})
-	LayerTypeZigbee    = gopacket.RegisterLayerType(1003, gopacket.LayerTypeMetadata{Name: "ZIGBEE", Decoder: gopacket.DecodeFunc(decodeZigbee)})
-	LayerTypeLorawan   = gopacket.RegisterLayerType(1004, gopacket.LayerTypeMetadata{Name: "LORAWAN", Decoder: gopacket.DecodeFunc(decodeLorawan)})
-	LayerTypeCan       = gopacket.RegisterLayerType(1005, gopacket.LayerTypeMetadata{Name: "CAN", Decoder: gopacket.DecodeFunc(decodeCan)})
-	LayerTypeModbusUdp = gopacket.RegisterLayerType(1006, gopacket.LayerTypeMetadata{Name: "MODBUSUDP", Decoder: gopacket.DecodeFunc(decodeModbusUdp)})
+	LayerTypeGoose   = gopacket.RegisterLayerType(1001, gopacket.LayerTypeMetadata{Name: "GOOSE", Decoder: gopacket.DecodeFunc(decodeGoose)})
+	LayerTypeBLE     = gopacket.RegisterLayerType(1002, gopacket.LayerTypeMetadata{Name: "BLE", Decoder: gopacket.DecodeFunc(decodeBLE)})
+	LayerTypeZigbee  = gopacket.RegisterLayerType(1003, gopacket.LayerTypeMetadata{Name: "ZIGBEE", Decoder: gopacket.DecodeFunc(decodeZigbee)})
+	LayerTypeLorawan = gopacket.RegisterLayerType(1004, gopacket.LayerTypeMetadata{Name: "LORAWAN", Decoder: gopacket.DecodeFunc(decodeLorawan)})
+	LayerTypeCan     = gopacket.RegisterLayerType(1005, gopacket.LayerTypeMetadata{Name: "CAN", Decoder: gopacket.DecodeFunc(decodeCan)})
 )
