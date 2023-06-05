69aaa2ab2f3156ef425b9a20d62a075c6a51f267
Samsul Hadi <samsul@usec.io>
2023-06-05 08:20:49 +0700

    chore: protocol name

--- streams/iec_60870_5.go
+++ streams/iec_60870_5.go
@@ -12,7 +12,7 @@ type IEC60870_5 struct {
 }
 
 func (t *IEC60870_5) Name() string {
-	return "IEC60870_5"
+	return "IEC 60870-5"
 }
 
 func (t *IEC60870_5) MarshalLogObject(enc zapcore.ObjectEncoder) error {
@@ -21,12 +21,14 @@ func (t *IEC60870_5) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 
 // Setup implements the Stream interface.
 func (t *IEC60870_5) Setup() error {
-	t.L.Info("IEC60870_5: parsing not implemented")
+	t.L.Info("IEC 60870-5: parsing not implemented")
 
 	return nil
 }
 
 func DetectIEC60870_5(payload []byte) bool {
+	// fmt.Printf("%x", payload)
+
 	if len(payload) < 6 || payload[0] == byte(0x68) {
 		return true
 	}
